acfd043cb8783b2fc5050f758faccf7810696606
Emanuel Cino <ecino@compassion.ch>
2023-10-16 15:33:52 +0200

    [ADD] theme_compassion
    Useful for quickly building a Compassion website
    
    - Contains blue theme
    - Contains default Compassion pictures in snippets
    - Contains Montserrat font

---
+++ theme_compassion/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Compassion Theme',
+    'description': 'Compassion Blue Theme',
+    'category': 'Theme/Services',
+    'summary': 'Compassion Blue Theme',
+    'sequence': 260,
+    'version': '1.0.0',
+    'author': 'Compassion CH',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/images_content.xml',
+    ],
+    'images': [
+        'static/description/compassion_screenshot.jpeg'
+    ],
+    'license': 'LGPL-3',
+}
