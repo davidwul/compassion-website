25c58a90f21bf2d80e09ca8f144102976549fd49
Emanuel Cino <ecino@compassion.ch>
2024-01-18 11:09:05 +0100

    [IMP] muskathlon: pre-commit execution

---
+++ muskathlon/__init__.py
@@ -0,0 +1,13 @@
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Sebastien Toth <popod@me.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import models
+from . import reports
+from . import controllers
+from . import forms
