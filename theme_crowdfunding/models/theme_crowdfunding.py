a8242dad9d9aa28092ab7b5ea2ee042bcc5098a4
Emanuel Cino <ecino@compassion.ch>
2024-04-22 15:55:40 +0200

    [IMP] theme_crowdfunding/: pre-commit execution

---
+++ theme_crowdfunding/models/theme_crowdfunding.py
@@ -0,0 +1,16 @@
+##############################################################################
+#
+#    Copyright (C) 2020 Compassion CH (http://www.compassion.ch)
+#    @author: Théo Nikles <theo.nikles@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+from odoo import models
+
+
+class ThemeCrowdfunding(models.AbstractModel):
+    _inherit = "theme.utils"
+
+    def _theme_crowdfunding_post_copy(self, mod):
+        self.disable_view("website_theme_install.customize_modal")
