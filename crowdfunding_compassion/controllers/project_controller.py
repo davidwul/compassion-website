f8dd218d1d2cc3a57ae301e7f85078b5d2d2ea68
Emanuel Cino <ecino@compassion.ch>
2024-05-16 12:20:06 +0200

    T1085 FIX CSP Url for TOGETHER and participant access

--- crowdfunding_compassion/controllers/project_controller.py
+++ crowdfunding_compassion/controllers/project_controller.py
@@ -71,7 +71,7 @@ class ProjectController(HomepageController):
             return request.redirect("/projects")
         sponsorships, donations = self.get_sponsorships_and_donations(
             participant.sponsorship_ids + participant.csp_sponsorship_ids,
-            participant.invoice_line_ids,
+            participant.sudo().invoice_line_ids,
         )
         values = {
             "participant": participant.sudo(),
