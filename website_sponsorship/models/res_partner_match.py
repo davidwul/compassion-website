741a31092c5cc8d16d92d6653fc9f4ebc3743a0c
Clément <100228798+clementcharmillot@users.noreply.github.com>
2024-08-08 11:00:32 +0200

    T1567 wordpress import (#32)
    * fix: add call to parent on exception
    
    * chore: format
    
    ---------
    
    Co-authored-by: Clément <clement.charmillot@gmail.com>

--- website_sponsorship/models/res_partner_match.py
+++ website_sponsorship/models/res_partner_match.py
@@ -16,23 +16,33 @@ class PartnerMatch(models.AbstractModel):
 
     def _match_email_and_name(self, vals):
         # Replace the rule with fuzzy search and using firstname and lastname
-        email = vals["email"].strip()
-        name = vals["firstname"] + " " + vals["lastname"]
-        return self.env["res.partner"].search(
-            [
-                ("name", "%", name),
-                ("email", "=ilike", email),
-            ],
-            limit=1,
-        )
+        try:
+            email = vals["email"].strip()
+            name = vals["firstname"] + " " + vals["lastname"]
+            return self.env["res.partner"].search(
+                [
+                    ("name", "%", name),
+                    ("email", "=ilike", email),
+                ],
+                limit=1,
+            )
+        except KeyError:
+            # No "firstname" or "lastname", the caller probably expected the initial
+            # behavior of the parent with "name"
+            return super()._match_email_and_name(vals)
 
     def _match_name_and_zip(self, vals):
         # Replace the rule for using firstname and lastname
-        name = vals["firstname"] + " " + vals["lastname"]
-        return self.env["res.partner"].search(
-            [
-                ("name", "ilike", name),
-                ("zip", "=", vals["zip"]),
-            ],
-            limit=1,
-        )
+        try:
+            name = vals["firstname"] + " " + vals["lastname"]
+            return self.env["res.partner"].search(
+                [
+                    ("name", "ilike", name),
+                    ("zip", "=", vals["zip"]),
+                ],
+                limit=1,
+            )
+        except KeyError:
+            # No "firstname" or "lastname", the caller probably expected the initial
+            # behavior of the parent with "name"
+            return super()._match_name_and_zip(vals)
